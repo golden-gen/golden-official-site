c9c8d8f31733a9232926081a748208425122cde6
adebabs-tec <ruqoyatomotayo@gmail.com>
2024-01-01 12:14:44 +0100

    blog page first commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,11 @@ module.exports = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      fontFamily: {
+        Roboto: ['Roboto', 'sans-serif'],
+        Manrope: ['Manrope', 'sans-serif'],
+        SpaceGro: ['Space Grotesk', 'sans-serif'],
+      },
     },
   },
   plugins: [],
