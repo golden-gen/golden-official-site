2b1a30f62c1239b31c10888e9e9e77e6252f1cb4
rahmlad-aramide <abdrahmanoladimeji02@gmail.com>
2024-03-02 23:33:16 +0100

    update: in progress

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,7 @@ const inter = Inter({ subsets: ['latin'] })
 export const metadata = {
   title: 'Golden Generation',
   description: 'Golden Generation Community Development Club Website',
+  metadataBase: new URL('https://golden-generation-oyo.netlify.app'),
 }
 
 export default function RootLayout({
